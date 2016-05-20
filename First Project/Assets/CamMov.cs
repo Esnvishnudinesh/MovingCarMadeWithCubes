40ba7922ba9e94e2c4baa405d3de7fdfd5967fce
Esnvishnudinesh <vishnudineshnv@gmail.com>
2016-05-20 14:44:53 +0530

    Commiting project
    Commiting project

---
+++ First Project/Assets/CamMov.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMov : MonoBehaviour {
+
+	public GameObject target;
+	public float offsetx,offsety,offsetz;
+	public float horspeed = 2.0F;
+	public float x = 0.0F;
+	public float y = 0.0F;
+
+	void Update () {
+
+		transform.position = target.transform.position+ new Vector3(offsetx,offsety,offsetz);
+		transform.LookAt (target.transform.position);
+		//transform.eulerAngles=target.transform.position+ new Vector3(offsetx,offsety,offsetz);
+		x = horspeed * Input.GetAxis ("Mouse X");
+		//y = horspeed * Input.GetAxis ("Mouse Y");
+		//float hv = horspeed * Input.GetAxis ("Mouse Z");
+		transform.Rotate (0.0f,x,0.0f);
+	//	transform.eulerAngles = new Vector3(y,x,0.0F);
+
+	}
+}
---
+++ First Project/Assets/CarDriver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDriver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += transform.forward * 5.0f * Time.deltaTime;
+
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				transform.position += transform.forward * 10.0f * Time.deltaTime;
+			}
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.position += transform.forward * -5.0f * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate (0.0f,-80.0f * Time.deltaTime,0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate (0.0f,80.0f * Time.deltaTime,0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.R)) {
+			transform.position += Vector3.up;
+			transform.rotation = Quaternion.LookRotation (transform.forward,Vector3.up);
+		}
+	}
+}
